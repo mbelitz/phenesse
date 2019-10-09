c6916ab2890a780d0ea1d47feddc2cbe39e32783
Michael Belitz <michaelbelitz06@gmail.com>
2019-10-09 10:28:59 -0400

    analytical solutions to bounds of parameterized distributions

--- R/weib_percentile.R
+++ R/weib_percentile.R
@@ -16,12 +16,11 @@
 #'
 #' @export
 #' @importFrom fitdistrplus fitdist
-#' @importFrom reconPlots curve_intersect
 #'
 #' @examples
 #'
 #' # Gather last 10 sightings of dodo
-#' observations <- c(1662, 1638, 1631, 1628, 1628, 1611, 1607, 1602, 1601, 1598)
+#' testobs <- c(150,160,162,164,168,170,172,176,178,188)
 #'
 #' # Estimate when 90% of dodo were left
 #' weib_percentile(observations, percentile = 0.9)
@@ -31,32 +30,59 @@
 
 weib_percentile <- function(observations, percentile = 0.9, iterations = 100){
 
-  cdf_orig <- function(observations){
-    weib <- fitdistrplus::fitdist(observations, distr = "weibull", method = "mle")
-    cdf <- 1 - exp(-(observations/weib$estimate['scale'])^weib$estimate['shape'])
-    return(cdf)
+  curve_intersect <- function(curve1, curve2, empirical=TRUE, domain=NULL) {
+    if (!empirical & missing(domain)) {
+      stop("'domain' must be provided with non-empirical curves")
+    }
+
+    if (!empirical & (length(domain) != 2 | !is.numeric(domain))) {
+      stop("'domain' must be a two-value numeric vector, like c(0, 10)")
+    }
+
+    if (empirical) {
+      # Approximate the functional form of both curves
+      curve1_f <- approxfun(curve1$x, curve1$y, rule = 2)
+      curve2_f <- approxfun(curve2$x, curve2$y, rule = 2)
+
+      # Calculate the intersection of curve 1 and curve 2 along the x-axis
+      point_x <- uniroot(function(x) curve1_f(x) - curve2_f(x),
+                         c(min(curve1$x), max(curve1$x)))$root
+
+      # Find where point_x is in curve 2
+      point_y <- curve2_f(point_x)
+    } else {
+      # Calculate the intersection of curve 1 and curve 2 along the x-axis
+      # within the given domain
+      point_x <- uniroot(function(x) curve1(x) - curve2(x), domain)$root
+
+      # Find where point_x is in curve 2
+      point_y <- curve2(point_x)
+    }
+
+    return(list(x = point_x, y = point_y))
   }
 
-  cdf_added <- function(observations){
-    weib_orig <- cdf_orig(observations)
+  create_cdf_ends <- function(observations){
     weib <- fitdistrplus::fitdist(observations, distr = "weibull", method = "mle")
+    cdf0 <- as.numeric(weib$estimate['scale']*(-log(1-0.01))^(1/weib$estimate['shape']))
+    cdf100 <- as.numeric(weib$estimate['scale']*(-log(1-0.99))^(1/weib$estimate['shape']))
 
-    added_vec <- sort(append(observations, values = c(min(observations - 15),
-                                                      max(observations + 15))), decreasing = FALSE)
+    added_vec <- sort(append(observations, values = c(cdf0, cdf100)), decreasing = FALSE)
     cdfadded <- 1 - exp(-(added_vec/weib$estimate['scale'])^weib$estimate['shape'])
-    return(cdfadded)
+    return(added_vec)
   }
 
   create_predict_df <- function(observations){
 
-    cdf <- cdf_orig(observations)
+    added_vec <- create_cdf_ends(observations)
+    vec_start <- min(added_vec)
+    vec_end <- max(added_vec)
+    new_vec <- seq(from = vec_start, to = vec_end, by = 0.5)
 
-    cdf_addedvec <- cdf_added(observations)
+    weib <- fitdistrplus::fitdist(observations, distr = "weibull", method = "mle")
+    cdfadded <- 1 - exp(-(new_vec/weib$estimate['scale'])^weib$estimate['shape'])
 
-    added_vec <- sort(append(observations, values = c(min(observations - 15),
-                                                      max(observations + 15))), decreasing = FALSE)
-
-    cdf_df <- data.frame(x = added_vec, y = cdf_addedvec)
+    cdf_df <- data.frame(x = new_vec, y = cdfadded)
     ends <- data.frame(x = c(min(added_vec - 1), max(added_vec+ 1)), y = c(-0.001,1.001))
     cdf_df <- rbind(cdf_df, ends)
     cdf_df <- cdf_df[order(cdf_df$x, decreasing = FALSE),]
@@ -73,7 +99,7 @@ weib_percentile <- function(observations, percentile = 0.9, iterations = 100){
       df1 <- create_predict_df(observations)
       sim_vector <- runif(n = length(observations),min = 0, max = 1)
       df2 <- data.frame(x = observations, y = sim_vector[i])
-      emptyvec[i] <-  reconPlots::curve_intersect(df1, df2)$x
+      emptyvec[i] <- curve_intersect(df1, df2)$x
     }
 
     new_vector <- sort(emptyvec, decreasing = FALSE)
@@ -86,7 +112,6 @@ weib_percentile <- function(observations, percentile = 0.9, iterations = 100){
 
     return(theta_hat_i)
   }
-
 
   theta_hat_df <- data.frame(x = observations, y = percentile)
 
