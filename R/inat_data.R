3f24b2e2817e1dd2fc4cd72a14dd6ceb68fb9968
Michael Belitz <michaelbelitz06@gmail.com>
2019-10-30 10:30:35 -0400

    initial commit

---
+++ R/inat_data.R
@@ -0,0 +1,33 @@
+#' iNaturalist data downloaded for the Washington DC area using the bounding box
+#' bounds = c(38, -77, 39, -76)
+#'
+#' Data was downloaded on 10/30/2019 for four species: Speyeria cybele, Danaus
+#' plexippus, Rudbeckia hirta, and Asclepias syriaca. These data were not scored
+#' to mark phenology, so all life stages/reproductive stages are included in the
+#' download.The download only includes 2019 observations and the doy (day of year)
+#' column was added post data download by MW Belitz using the lubridate package.
+#'
+#' @docType data
+#'
+#' @usage data(inat_obs)
+#'
+#' @format A data frame with 252 rows and 6 variables:
+#' \describe{
+#'    \item{scientific_name}{binomial of species}
+#'    \item{latitude}{latitude where observations occurred}
+#'    \item{longitude}{longitude where observations occurred}
+#'    \item{common_name}{common name related to species}
+#'    \item{observed_on}{original date listed of observation}
+#'    \item{doy}{day of year when the observation occurred, variable created by MW Belitz
+#'    using the package lubridate}
+#'    }
+#'
+#' @keywords datasets
+#'
+#' @references www.inaturalist.org
+#' @source \href{https://inaturalist.org}
+#'
+#' @examples
+#' data(inat_obs)
+#' inat_obs <- attr(inat_obs)
+#' View(inat_obs)
